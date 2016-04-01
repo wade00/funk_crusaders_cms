0f7fcfe6e1f0274827466861ec8e9a6acca3a961
Wade Novak <wade@launchpadlab.com>
2016-04-01 17:36:46 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+ruby '2.2.3'
+
+gem 'rails', '4.2.5'
+gem 'locomotivecms', '~> 3.0.0'
+gem 'sass-rails', '~> 5.0'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+
+# Use Capistrano for deployment
+# gem 'capistrano-rails', group: :development
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'byebug'
+  gem 'pry-nav'
+  gem 'pry-rails'
+  gem 'pry-stack_explorer'
+  gem 'pry-theme'
+  gem 'spring'
+end
+
+gem 'carrierwave-aws'
---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,22 @@
+# Configure
+Dragonfly.app(:engine).configure do
+  plugin :imagemagick,
+    convert_command:  `which convert`.strip.presence || '/usr/local/bin/convert',
+    identify_command: `which identify`.strip.presence || '/usr/local/bin/identify'
+
+  verify_urls true
+
+  secret '459ab8d8117365b4024964a48bf5790256ac580e9a0edd8e0f8505ef60fb8125'
+
+  url_format '/images/dynamic/:job/:sha/:basename.:ext'
+
+  fetch_file_whitelist /public/
+
+  fetch_url_whitelist /.+/
+end
+
+# Logger
+Dragonfly.logger = Rails.logger
+
+# Mount as middleware
+Rails.application.middleware.use Dragonfly::Middleware, :engine
